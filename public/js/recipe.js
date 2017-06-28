12122d7aa616fd521087bf9969db8af0085b779d
Dvir Edri <dviredri433@gmail.com>
2017-06-28 16:04:08 +0300

    bowerInstall

--- public/js/recipe.js
+++ public/js/recipe.js
@@ -5,19 +5,6 @@ $(document).on('change', ':file', function() {
     input.trigger('fileselect', [numFiles, label]);
 });
 
-$(document).ready( function() {
-    $(':file').on('fileselect', function(event, numFiles, label) {
-        console.log(numFiles);
-        console.log(label);
-    });
-});
-$(document).on('change', ':file', function() {
-    var input = $(this),
-        numFiles = input.get(0).files ? input.get(0).files.length : 1,
-        label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
-    input.trigger('fileselect', [numFiles, label]);
-});
-
 console.log("hello")
 
 // collect the information from the recpie from
@@ -27,10 +14,10 @@ newRecipe.recipeName=$("#recipeNameInput").val();
 newRecipe.totalTimeInSeconds=$("#cookingTimeInput").val();
 newRecipe.cuisine="Mexican";
 newRecipe.ingredients=$("#recipeInstuctionsInput").val();
-// newRecipe.imageAddress=$(".imageSourceInput").val();
-newRecipe.imageRecipe="C:\Users\dvir\Desktop\db music";
+newRecipe.imageAddress=$(".imageSourceInput").val();
+// newRecipe.imageRecipe="C:\Users\dvir\Desktop\db music";
 console.log(newRecipe);
-saveNewRecipe(newRecipe);
+// saveNewRecipe(newRecipe);
 });
 
   function saveNewRecipe(newRecipe) {
