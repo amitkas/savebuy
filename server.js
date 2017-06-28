27d01053a9f2274eddbdc2168167cc312b4f79a5
Dvir Edri <dviredri433@gmail.com>
2017-06-28 17:56:48 +0300

    clockProgress

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(express.static('public'));
 app.use(express.static('node_modules'));
 app.use(bodyParser.json());
 app.use(express.static('bower_components'));
+app.use(express.static('clockpicker-gh-pages'));
 app.use(bodyParser.urlencoded({
   extended: false
 }));
